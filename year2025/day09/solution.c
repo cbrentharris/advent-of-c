0b37e59075df2cb855f2bbc3702e777c4423390f
Christopher Harris <cbrentharris@gmail.com>
2025-12-09 09:08:48 -0600

    Day 9 scaffold

---
+++ year2025/day09/solution.c
@@ -0,0 +1,15 @@
+#include "aoc.h"
+#include "solution.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER_SIZE 512
+
+long day09_part1(FILE *fp) {
+	return 0;
+}
+
+long day09_part2(FILE *fp) {
+	return 0;
+}
---
+++ year2025/day09/solution.h
@@ -0,0 +1,9 @@
+#ifndef SOLUTION_2025_DAY09_H
+#define SOLUTION_2025_DAY09_H
+#include <stddef.h>
+#include <stdio.h>
+
+long day09_part1(FILE *fp);
+long day09_part2(FILE *fp);
+
+#endif
