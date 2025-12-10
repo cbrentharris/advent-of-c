29dd207fac43310148d14cb4f0ce0510a5211302
Christopher Harris <cbrentharris@gmail.com>
2025-12-10 13:53:19 -0600

    Day 10 scaffold

---
+++ year2025/day10/solution.c
@@ -0,0 +1,14 @@
+#include "aoc.h"
+#include "solution.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+long day10_part1(FILE *fp) {
+	return 0;
+}
+
+long day10_part2(FILE *fp) {
+	return 0;
+}
+
---
+++ year2025/day10/solution.h
@@ -0,0 +1,9 @@
+#ifndef SOLUTION_2025_DAY10_H
+#define SOLUTION_2025_DAY10_H
+#include <stddef.h>
+#include <stdio.h>
+
+long day10_part1(FILE *fp);
+long day10_part2(FILE *fp);
+
+#endif
